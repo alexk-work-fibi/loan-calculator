fd3cffb502eea63dd80b0bfcf7bfc81ec57c65b2
Your Name <you@example.com>
2025-05-22 14:39:41 +0300

    Add loan-calculator-ui Angular application

---
+++ loan-calculator-ui/src/app/components/payment-schedule/payment-schedule.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-schedule',
+  imports: [],
+  templateUrl: './payment-schedule.component.html',
+  styleUrl: './payment-schedule.component.scss'
+})
+export class PaymentScheduleComponent {
+
+}
